99148c9f025168c7034506c0b6dfef6917d932e5
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-02-01 16:30:33 +0530

    Create shy.c

---
+++ shy.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5;
+    int b= 6;
+    printf("%d",'Z');
+    getch();
+    
+}
+
+    int add(int x,int y)
+    {
+        int Z;
+        printf("%d",'Z');
+        return Z;
+    }
