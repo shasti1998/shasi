bc9ada0d712f8ee6e804701201137e559042ae6f
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-02-01 15:57:12 +0530

    Update raju.c

--- raju.c
+++ raju.c
@@ -3,13 +3,13 @@
 int main()
 {
     char ch;
-    
+
     /* Input a character from user */
     printf("Enter any character: ");
     scanf("%c", &ch);
-    
 
-    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+
+    if((ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90))
     {
         printf("Character is an ALPHABET.");
     }
