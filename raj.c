7833d6b98917a16dca904516b64d7ac643e35100
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-02-01 16:49:49 +0530

    Create raj.c

---
+++ raj.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a=25;
+  fun('A')
+  printf("%d",'A');
+  getch();
+}
+int fun (int 's')
+{
+    s=10;
+    printf("%d",'s');
+    return 0;
+}
