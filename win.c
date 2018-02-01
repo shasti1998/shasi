71005a5c36b950f102cfbb2a7d517e616d5029a7
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-02-01 16:16:41 +0530

    Create win.c

---
+++ win.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int a [3];
+    printf("enter the values");
+    for(i=0;i<0;i++)
+    {
+        scanf("%d", & a[i]);
+        
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=1;j<3;j++)
+    }
+    {
+        if(a(i)>a(i))
+        a(o)>a(2)
+    }
+    { 
+        int t;
+        t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+}
+
+
